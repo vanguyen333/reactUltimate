69ce30c4acc01b9b0f44914853911b8c953ded9b
vanguyen333 <vanguyen333@yahoo.com>
2023-05-11 22:29:40 -0400

    switch to using arrow functions to eliminate bugs

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -10,11 +10,11 @@ class MyComponent extends React.Component {
     age: 0,
   };
 
-  handleClick(event) {
+  handleClick = (event) => {
     console.log(">>Click me my button");
     console.log(`My name is`, this.state.name);
     // console.log(event);
-  }
+  };
 
   handleOnMouseOver(event) {
     console.log(event.pageX);
@@ -25,7 +25,13 @@ class MyComponent extends React.Component {
       <div>
         My name is {this.state.name} and I'm from {this.state.address}
         <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
-        <button onClick={this.handleClick}>Click me</button>
+        <button
+          onClick={(event) => {
+            this.handleClick(event);
+          }}
+        >
+          Click me
+        </button>
       </div>
     );
   }
