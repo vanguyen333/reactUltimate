17cc71bfd650b63d88f28709480719832c2bae20
vanguyen333 <vanguyen333@yahoo.com>
2023-05-12 11:24:54 -0400

    finish clip 26 form with react, added event.prentDefault();//prevent page from being reloaded

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -36,13 +36,17 @@ class MyComponent extends React.Component {
       name: event.target.value,
     });
   };
+  handleOnSubmit = (event) => {
+    event.preventDefault(); //stop reloading page after pressing ok button from alert me
+    console.log(this.state);
+  };
   //JSX ...
   render() {
     return (
       <div>
         {/* My name is {this.state.name} and I'm from {this.state.address} */}
         My name is {this.state.name} and my age is {this.state.age}
-        <form>
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
           <input
             type="text"
             onChange={(event) => this.handleOnChangeInput(event)}
