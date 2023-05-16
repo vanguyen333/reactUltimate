1bff078429f14057081dc4a3ce15c991ddcfa092
vanguyen333 <vanguyen333@yahoo.com>
2023-05-16 18:11:02 -0400

    if true, will print users, if false, will hide users

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,50 +1,50 @@
+/* eslint-disable no-lone-blocks */
 import React from "react";
 
 class DisplayInfor extends React.Component {
+  handleShowHide = () => {
+    alert("me");
+  };
   render() {
     //destructuring array/object
 
     const { listUsers } = this.props; //object
     // console.table(listUsers); //print users as table
     //   console.log(listUsers);
+    // console.table(listUsers);
     return (
       <div>
-        {listUsers.map((user, index) => {
-          console.log(">>>Check map user:", user);
-          //the plus + sign will convert string to number
-          {
-            return (
-              <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                <div>My name's {user.name}</div>
-                <div>My age's {user.age} </div>
-              </div>
-            );
-
-            /* if (+user.age > 18) {
-            return (
-              <div key={user.id} className="green">
-                <div>My name's {user.name}</div>
-                <div>My age's {user.age} </div>
-              </div>
-            );
-          } else {
-            return (
-              <div key={user.id} className="red">
-                <div>My name's {user.name}</div>
-                <div>My age's {user.age} </div>
-              </div>
-            );
-          } */
-          }
-        })}
-        {/* <div>My name's {name}</div>
-        <div>My age's {age}</div>
-        <hr />
-        <div>My name's {name}</div>
-        <div>My age's {age}</div>
-        <hr />
-        <div>My name's {name}</div>
-        <div>My age's {age}</div> */}
+        <div>
+          <span
+            onClick={() => {
+              this.handleShowHide();
+            }}
+          >
+            Hide list users:
+          </span>
+        </div>
+        {/* if false, will hide users */}
+        {false && (
+          <div>
+            {listUsers.map((user, index) => {
+              console.log(">>>Check map user:", user);
+              //the plus + sign will convert string to number
+              {
+                return (
+                  <div>
+                    <div
+                      key={user.id}
+                      className={+user.age > 18 ? "green" : "red"}
+                    >
+                      <div>My name's {user.name}</div>
+                      <div>My age's {user.age} </div>
+                    </div>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        )}
       </div>
     );
   }
