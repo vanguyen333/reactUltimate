80e1a928706e09cdeffa9f2539547a45ad602978
vanguyen333 <vanguyen333@yahoo.com>
2023-05-18 15:59:47 -0400

    click show list users - alert me - ok, the list of users hidden

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,93 +1,106 @@
 /* eslint-disable no-lone-blocks */
-import React from "react";
+import React, { useState } from "react";
 import "./DisplayInfor.scss";
 import logo from "./../logo.svg";
 
-class DisplayInfor extends React.Component {
-  constructor(props) {
-    console.log(">>>Call constructor: 1");
-    super(props);
-    this.state = {
-      isShowListUser: true,
-    };
-  }
+//statless vs statefull
+// class DisplayInfor extends React.Component {
+//   render() {
+//     console.log("Call me render");
+//     //destructuring array/object
 
-  componentDidMount() {
-    console.log(">>>call me component did mount");
-    setTimeout(() => {
-      document.title = "Van & Green Pear";
-    }, 3000);
-  }
+//     const { listUsers } = this.props; //object
+//     // console.table(listUsers); //print users as table
+//     //   console.log(listUsers);
+//     // console.table(listUsers);
+//     return (
+//       <div className="display-infor-container">
+//         {/* <img src={logo} /> */}
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log(">>>call me componentDidUpdate", this.props, prevProps);
-    if (this.props.listUsers !== prevProps.listUsers) {
-      alert("You got 5 users");
-    }
-  }
+//         {/* if false, will hide users */}
+//         {true && (
+//           <>
+//             {listUsers.map((user, index) => {
+//               {
+//                 /* console.log(">>>Check map user:", user); */
+//               }
+//               //the plus + sign will convert string to number
+//               {
+//                 return (
+//                   <div>
+//                     <div
+//                       key={user.id}
+//                       className={+user.age > 18 ? "green" : "red"}
+//                     >
+//                       <div>My name's {user.name}</div>
+//                       <div>My age's {user.age} </div>
+//                     </div>
+//                     <di>
+//                       <button
+//                         onClick={() => this.props.handleDeleteUser(user.id)}
+//                       >
+//                         Delete
+//                       </button>
+//                     </di>
+//                     <hr />
+//                   </div>
+//                 );
+//               }
+//             })}
+//           </>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
-  handleShowHide = () => {
-    this.setState({
-      isShowListUser: !this.state.isShowListUser, //! is like a toggle, on and off
-    });
+const DisplayInfor = (props) => {
+  const { listUsers } = props; //object
+  const [isShowHideListUser, setShowHideListUser] = useState(true);
+  // this.state = {
+  //   isShowHideListUser: true,
+  // };
+
+  const handleShowHideListUser = () => {
+    alert("click me");
+    setShowHideListUser(!isShowHideListUser);
   };
-  render() {
-    console.log("Call me render");
-    //destructuring array/object
-
-    const { listUsers } = this.props; //object
-    // console.table(listUsers); //print users as table
-    //   console.log(listUsers);
-    // console.table(listUsers);
-    return (
-      <div className="display-infor-container">
-        {/* <img src={logo} /> */}
-        <div>
-          <span
-            onClick={() => {
-              this.handleShowHide();
-            }}
-          >
-            {this.state.isShowListUser === true
-              ? "Hide list users:"
-              : "Show list users:"}
-          </span>
-        </div>
-        {/* if false, will hide users */}
-        {this.state.isShowListUser && (
-          <>
-            {listUsers.map((user, index) => {
-              {
-                /* console.log(">>>Check map user:", user); */
-              }
-              //the plus + sign will convert string to number
-              {
-                return (
-                  <div>
-                    <div
-                      key={user.id}
-                      className={+user.age > 18 ? "green" : "red"}
-                    >
-                      <div>My name's {user.name}</div>
-                      <div>My age's {user.age} </div>
-                    </div>
-                    <di>
-                      <button
-                        onClick={() => this.props.handleDeleteUser(user.id)}
-                      >
-                        Delete
-                      </button>
-                    </di>
-                    <hr />
-                  </div>
-                );
-              }
-            })}
-          </>
-        )}
+  return (
+    <div className="display-infor-container">
+      <div>
+        <span onClick={() => handleShowHideListUser()}>Show list users</span>
       </div>
-    );
-  }
-}
+      {isShowHideListUser && (
+        <>
+          {listUsers.map((user, index) => {
+            {
+              /* console.log(">>>Check map user:", user); */
+            }
+            //the plus + sign will convert string to number
+            {
+              return (
+                <div>
+                  <div
+                    key={user.id}
+                    className={+user.age > 18 ? "green" : "red"}
+                  >
+                    <div>My name's {user.name}</div>
+                    <div>My age's {user.age} </div>
+                  </div>
+                  <di>
+                    <button onClick={() => props.handleDeleteUser(user.id)}>
+                      Delete
+                    </button>
+                  </di>
+                  <hr />
+                </div>
+              );
+            }
+          })}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default DisplayInfor;
