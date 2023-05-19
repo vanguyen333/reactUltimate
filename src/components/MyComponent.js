877d14c9e97730f91c914ef417e3e17f173ccb77
vanguyen333 <vanguyen333@yahoo.com>
2023-05-19 09:51:47 -0400

    changed Mycomponent class to function component using Hook

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,70 +2,104 @@
 //function component
 //merge state only occurs with react => class
 
-import React from "react";
+import React, { useState } from "react";
 import AddUserInfor from "./AddUserInfor";
 import DisplayInfor from "./DisplayInfor";
 
-class MyComponent extends React.Component {
-  state = {
-    listUsers: [
-      { id: 1, name: "Green Pear", age: "13" },
-      { id: 2, name: "Van", age: "26" },
-      { id: 3, name: "Ngoc", age: "69" },
-    ],
+const MyComponent = (props) => {
+  const [listUsers, setListUsers] = useState([
+    { id: 1, name: "Green Pear", age: "13" },
+    { id: 2, name: "Van", age: "26" },
+    { id: 3, name: "Ngoc", age: "69" },
+  ]);
+
+  const handleAddNewUser = (userObj) => {
+    setListUsers([userObj, ...listUsers]);
   };
 
-  handleAddNewUser = (userObj) => {
-    // let listUsersNew = this.state.listUsers;
-    // listUsersNew.unshift(userObj);
-    // console.log("check:", listUsersNew);
-    // this.setState({
-    //   listUsers: listUsersNew,
-    // });
-
-    // this.setState({
-    //   listUsers: [userObj, ...this.state.listUsers],
-    // });
-    this.setState({
-      listUsers: [userObj, ...this.state.listUsers],
-    });
-  };
-
-  handleDeleteUser = (userId) => {
-    let listUsersClone = [...this.state.listUsers];
+  const handleDeleteUser = (userId) => {
+    let listUsersClone = listUsers;
     listUsersClone = listUsersClone.filter((item) => item.id !== userId);
-    this.setState({
-      listUsers: listUsersClone,
-    });
+    setListUsers(listUsersClone);
   };
-  //JSX ...
-  render() {
-    //Dry : dont repeat yourself
-    // const test = { name: " Green Pear ", age: 45 };
-    return (
-      <>
-        {/* {test.name}
-        {test.age} */}
-        {/* {console.log("Check test: ", test)} */}
-        {/* {JSON.stringify(test)} */}
+  return (
+    <>
+      <br />
+      <div className="a">
+        <AddUserInfor handleAddNewUser={handleAddNewUser} />
         <br />
-        <div className="a">
-          {/* My name is {this.state.name} and I'm from {this.state.address} */}
+        <br />
+        <DisplayInfor
+          listUsers={listUsers}
+          handleDeleteUser={handleDeleteUser}
+        />
+        <hr />
+      </div>
+      <div className="b"></div>
+    </>
+  );
+};
 
-          <AddUserInfor handleAddNewUser={this.handleAddNewUser} />
+//class MyComponent extends React.Component {
+//   state = {
+//     listUsers: [
+//       { id: 1, name: "Green Pear", age: "13" },
+//       { id: 2, name: "Van", age: "26" },
+//       { id: 3, name: "Ngoc", age: "69" },
+//     ],
+//   };
 
-          <br />
-          <br />
-          <DisplayInfor
-            listUsers={this.state.listUsers}
-            handleDeleteUser={this.handleDeleteUser}
-          />
-          <hr />
-        </div>
-        <div className="b"></div>
-      </>
-    );
-  }
-}
+//   handleAddNewUser = (userObj) => {
+//     // let listUsersNew = this.state.listUsers;
+//     // listUsersNew.unshift(userObj);
+//     // console.log("check:", listUsersNew);
+//     // this.setState({
+//     //   listUsers: listUsersNew,
+//     // });
+
+//     // this.setState({
+//     //   listUsers: [userObj, ...this.state.listUsers],
+//     // });
+//     this.setState({
+//       listUsers: [userObj, ...this.state.listUsers],
+//     });
+//   };
+
+//   handleDeleteUser = (userId) => {
+//     let listUsersClone = [...this.state.listUsers];
+//     listUsersClone = listUsersClone.filter((item) => item.id !== userId);
+//     this.setState({
+//       listUsers: listUsersClone,
+//     });
+//   };
+//   //JSX ...
+//   render() {
+//     //Dry : dont repeat yourself
+//     // const test = { name: " Green Pear ", age: 45 };
+//     return (
+//       <>
+//         {/* {test.name}
+//         {test.age} */}
+//         {/* {console.log("Check test: ", test)} */}
+//         {/* {JSON.stringify(test)} */}
+//         <br />
+//         <div className="a">
+//           {/* My name is {this.state.name} and I'm from {this.state.address} */}
+
+//           <AddUserInfor handleAddNewUser={this.handleAddNewUser} />
+
+//           <br />
+//           <br />
+//           <DisplayInfor
+//             listUsers={this.state.listUsers}
+//             handleDeleteUser={this.handleDeleteUser}
+//           />
+//           <hr />
+//         </div>
+//         <div className="b"></div>
+//       </>
+//     );
+//   }
+// }
 
 export default MyComponent;
