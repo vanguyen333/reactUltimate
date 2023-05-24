034a11382a2da031162cca8d2b9b01acbaa3138b
vanguyen333 <vanguyen333@yahoo.com>
2023-05-23 20:16:38 -0400

    installed bootstrap, react-bootstrap, react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.scss";
 
 const App = () => {
-  return <div className="App">Hello world</div>;
+  return <div className="app-container">Hello world</div>;
 };
 
 export default App;
