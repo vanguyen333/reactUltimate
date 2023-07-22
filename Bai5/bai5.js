803339295e361c7906b85b2da4d48733d7774232
vanguyen333 <vanguyen333@yahoo.com>
2023-07-21 21:56:23 -0400

    filter, done lesson 5

--- Bai5/bai5.js
+++ Bai5/bai5.js
@@ -6,11 +6,11 @@
 //syntax: array.map(function(currentValue,index,arr),thisValue)
 const myArray = ["apple", "banana", "orange"];
 const myList = myArray.map((item) => `<p>${item}</p>`);
-console.log(myList, myArray);
+// console.log(myList, myArray);
 
 const myArray1 = [1, 2, 3];
 const myList1 = myArray1.map((item) => item * 2);
-console.log(myList1, myArray1);
+// console.log(myList1, myArray1);
 
 const myArray2 = [1, 2, 3];
 //inside map is a function, const a = () =>{}
@@ -18,4 +18,19 @@ const myList2 = myArray2.map((item, index) => {
   console.log(item, index);
   return item * 2;
 });
-console.log(myList2, myArray2);
+// console.log(myList2, myArray2);
+
+//filter: loc ra item from the array, not change the original array,
+//filter() not execute the function for empty elements
+const ages = [32, 33, 16, 40];
+//bad code
+// const result = ages.filter(checkAdult);
+// function checkAdult(age) {
+//   return age >= 18;
+// }
+//good code
+const result = ages.filter((item) => {
+  console.log(item); //tra ra 32,33,16,40
+  return item >= 18; //tra ra 32,33,40
+});
+console.log(result);
