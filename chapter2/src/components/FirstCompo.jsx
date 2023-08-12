37d963843e5ef4728a88da521ae852fd6d886583
vanguyen333 <vanguyen333@yahoo.com>
2023-08-11 22:39:20 -0400

    basic event, chapter2 #19 DOM events

--- chapter2/src/components/FirstCompo.jsx
+++ chapter2/src/components/FirstCompo.jsx
@@ -4,13 +4,31 @@ const FirstComp = () => {
     address: "vanguyendev",
     age: 32,
   };
+  const handleClick = (event) => {
+    // console.log("click me");
+    console.log(event.target);
+  };
+  const handleOnMoverOver = () => {
+    console.log(event);
+  };
+
   return (
     <div>
       <h1>
-        My name is {state.name} and my channel is {state.address}, I am{" "}
-        {state.age} years of age.
+        My name is {state.name} and my channel is {state.address}
       </h1>
-      {/* hi there */}
+      <button
+        onMouseOver={handleOnMoverOver}
+        className="border bg-blue-400 rounded py-2 px-4"
+      >
+        Hover me
+      </button>
+      <button
+        onClick={handleOnMoverOver}
+        className="border bg-blue-400 rounded py-2 px-4"
+      >
+        Click me
+      </button>
     </div>
   );
 };
