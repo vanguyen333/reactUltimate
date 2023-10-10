8d242a74efce5ed115fb41493281390e20ffaa34
vanguyen333 <vanguyen333@yahoo.com>
2023-10-10 10:17:34 -0400

    Chapter 3 #38 useEffect hook

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,47 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./DisplayInfor.scss";
 import logo from "./../logo.svg";
-
-//stateless vs statefull
-// class DisplayInfor extends React.Component {
-//   render() {
-//     console.log(">>>Call me render");
-//     // console.log(this.props);
-//     //Destructuring array/object
-//     const { listUsers } = this.props; //object
-//     console.log(listUsers);
-//     // console.table(listUsers);
-//     // === const listUsers = this.props.listUsers;
-//     //prop = >properties thuoc tinh, truyen du lieu from dad to kids
-//     return (
-//       <div className="display-infor-container">
-//         {true && (
-//           <>
-//             {listUsers.map((user, index) => {
-//               console.log(">>check map user:", user);
-//               return (
-//                 //   {/* the +m sign is to transform age of 18 string to number */}
-//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                   <div>My name is {user.name} </div>
-//                   <div>My age is {user.age}</div>
-//                   <div>
-//                     <button
-//                       onClick={() => this.props.handleDeleteUser(user.id)}
-//                     >
-//                       Delete
-//                     </button>
-//                   </div>
-//                   <hr />
-//                 </div>
-//               );
-//             })}
-//             <hr />
-//           </>
-//         )}
-//       </div>
-//     );
-//   }
-// }
 
 const DisplayInfor = (props) => {
   const { listUsers } = props; //object
@@ -51,6 +10,13 @@ const DisplayInfor = (props) => {
     alert("click me");
     setShowHideListuser(!isShowHideListuser);
   };
+  console.log(">>>call me render");
+  useEffect(() => {
+    if (listUsers.length === 0) {
+      alert("you deleted all the users");
+    }
+    console.log(">>>call me useEffect");
+  }, [listUsers]);
   return (
     <div className="display-infor-container">
       <div>
@@ -61,7 +27,9 @@ const DisplayInfor = (props) => {
       {isShowHideListuser && (
         <>
           {listUsers.map((user, index) => {
-            console.log(">>check map user:", user);
+            {
+              /* console.log(">>check map user:", user); */
+            }
             return (
               //   {/* the +m sign is to transform age of 18 string to number */}
               <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
