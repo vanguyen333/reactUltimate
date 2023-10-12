9732d9c6fe85c4ff80d04cde5f851759cb9cb01a
vanguyen333 <vanguyen333@yahoo.com>
2023-10-12 11:03:57 -0400

    installed bootstrap and react router dom

--- src/App.js
+++ src/App.js
@@ -5,32 +5,9 @@ import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 import MyComponent from "./components/MyComponent";
 
 import React from "react";
-class App extends React.Component {
-  //JSX
-  render() {
-    return (
-      <div className="">
-        <MyComponent />
-      </div>
-    );
-  }
-}
 
-// const App = () => {
-//   const count = useSelector((state) => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>Hoi Dan IT</p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// };
+const App = () => {
+  return <div className="App">hello world</div>;
+};
 
 export default App;
