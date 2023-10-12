88c37a1570adedcdcff3928a39d5e24c79896f8e
vanguyen333 <vanguyen333@yahoo.com>
2023-10-12 11:08:54 -0400

    Chapter 5 #46 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-// import logo from "./logo.svg";
 import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
-
-import React from "react";
 
 const App = () => {
-  return <div className="App">hello world</div>;
+  return <div className="app-container">hello world</div>;
 };
 
 export default App;
